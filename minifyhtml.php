cf9200c49a505bdbddd02a61979ce7c90fdccaee
Ramon Garcia <ramon@dred.com>
2016-04-01 15:37:44 +0200

    Moved files

--- minifyhtml.php
+++ minifyhtml.php
@@ -1,11 +1,12 @@
 <?php
-if (c::get('MinifyHTML') !== FALSE):
-	require_once 'library/Minify_HTML.php';
-	
+
+if (!c::get('MinifyHTML')) {
+	require_once(__DIR__ . DS . 'library' . DS . 'Minify_HTML.php');
+
 	function minifyhtml($buffer)
 	{
 	    return Minify_HTML::minify($buffer);
 	}
-	
+
 	ob_start('minifyhtml');
-endif;
+}
